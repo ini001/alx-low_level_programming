afe88d98d1a72e201c5c2de9107fbe7ba869a07b
ini001.inioluwa <ini001.iniadedoyin@gmail>
2023-02-20 15:31:36 -0800

    prints all possible different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+				if (i == 7 && j == 8 && k == 9)
+				{
+					;
+				}
+				else
+				{
+					putchar(',');
+				}
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
