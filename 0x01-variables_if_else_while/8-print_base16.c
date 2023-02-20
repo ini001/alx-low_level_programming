d313a62d1822d7be35087dd97e89ec3650f0fb74
ini001.inioluwa <ini001.iniadedoyin@gmail>
2023-02-20 05:28:38 -0800

    print all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char low;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	for (low = 'a'; low <= 'f'; low++)
+	{
+		putchar(low);
+	}
+	putchar('\n');
+}
+
