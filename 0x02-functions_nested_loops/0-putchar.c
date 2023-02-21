43631885d206cd6aad4550628927940260e13a21
ini001.inioluwa <ini001.iniadedoyin@gmail>
2023-02-21 06:45:51 -0800

    a program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 #include "main.h"
-
+#include "_putchar"
 /**
  * main - Check description
  * Description: It prints the word Holberton, followed by a new line.
@@ -8,13 +8,16 @@
  */
 int main(void)
 {
-	char word[8] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		putchar(word[i]);
-		putchar('\n');
-	}
+	char word = "_putchar";
+	/*
+	 * char word[8] = "_putchar";
+	 * int i;
+	 * for (i = 0; i < 8; i++)
+	 * {
+	 * _putchar(word[i]);
+	 * _putchar('\n');
+	 * }
+	 */
+	putc(word, stdout);
 	return (0);
 }
