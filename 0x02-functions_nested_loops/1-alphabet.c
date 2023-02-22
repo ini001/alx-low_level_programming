c30e932f177492c011ff663b516d554e0a6b5ab9
ini001.inioluwa <ini001.iniadedoyin@gmail>
2023-02-21 20:30:54 -0800

    prints 10 times the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,9 @@
 #include "main.h"
-
 /**
- * print alphabet - print alphas in lowercase
+ * main print alphabet - print alphabet in lowercase
+ * Description: It prints the alphabet in lowercase, followed by a new line.
+ * Return:0 (success)
+ *
  */
 void print_alphabet(void)
 {
